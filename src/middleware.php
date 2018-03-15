1fb6944a05cb1e49ae3fdf99a2e7aa21ad818266
Scott Yacko <scott@yackofamily.com>
2018-03-15 12:37:17 -0700

    Regen composer.json and add API Auth
    Regenered composer.json to include class changes for middleware additions.
    Added class Middleware to middleware.php to validate access token.

--- src/middleware.php
+++ src/middleware.php
@@ -1,27 +1,122 @@
 <?php
 // Application middleware
-
 // e.g: $app->add(new \Slim\Csrf\Guard);
-
 
 use Auth0\SDK\JWTVerifier;
 use Auth0\SDK\Helpers\Cache\FileSystemCacheHandler;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\ResponseInterface;
-use Interop\Container\ContainerInterface;
+use Auth0\SDK\Exception\CoreException;
+use Auth0\SDK\Exception\InvalidTokenException;
 
-$headers =  getallheaders();
-echo $headers['Authorization'], "\n\n\n";
+class Middleware
+{
+	protected $container;
 
-$verifier = new JWTVerifier([
-	'valid_audiences' => ['https://192.168.41.75/edeninfo'],
-	'authorized_iss' => ['https://spotlightmartdev.auth0.com/'],
-	'supported_algs' => ['RS256'],
-	'cache' => new FileSystemCacheHandler() // This parameter is optional. By default no cache is used to fetch the Json Web Keys.
+	public function __construct($container)
+	{
+		$this->container = $container;
+	}
 
-]);
+	/**
+	 * Short Description
+	 *
+	 * Long Description
+	 *
+	 */
+	public function __invoke($request, $response, $next)
+	{
+//		$this->myLogger->debug(__METHOD__);
 
-$decoded = $verifier->verifyAndDecode($headers['Authorization']);
+		$headers = getallheaders();
+		$myResponse = $response;
 
-var_dump($decoded);
+		try
+		{
+			$verifier = new JWTVerifier(['valid_audiences' => ['https://192.168.41.75/edeninfo'],
+			                             'authorized_iss'  => ['https://spotlightmartdev.auth0.com/'],
+			                             'supported_algs'  => ['RS256']
+			]);
+			$verifier->verifyAndDecode($headers['Authorization']);
+		} catch (InvalidTokenException $e)
+		{
+			return $myResponse->withJson(array('errCode'    => 401,
+			                                   'statusText' => $e->getMessage(),
+			                                   'codeLoc'    => __METHOD__,
+			                                   'custMsg'    => '',
+			                                   'retPack'    => array('token' => $headers['Authorization'])));
+		} catch (CoreException $e)
+		{
+			return $myResponse->withJson(array('errCode'    => 401,
+			                                   'statusText' => $e->getMessage(),
+			                                   'codeLoc'    => __METHOD__,
+			                                   'custMsg'    => '',
+			                                   'retPack'    => array('token' => $headers['Authorization'])));
+		}
 
+		$myResponse = $next($request, $response);
+
+		return $myResponse;
+	}
+}
+
+
+
+
+//use Auth0\SDK\JWTVerifier;
+//use Auth0\SDK\Helpers\Cache\FileSystemCacheHandler;
+//use Psr\Http\Message\ServerRequestInterface;
+//use Psr\Http\Message\ResponseInterface;
+//use Interop\Container\ContainerInterface;
+
+/**
+ * Example middleware closure
+ *
+ * @param  \Psr\Http\Message\ServerRequestInterface $request  PSR7 request
+ * @param  \Psr\Http\Message\ResponseInterface      $response PSR7 response
+ * @param  callable                                 $next     Next middleware
+ *
+ * @return \Psr\Http\Message\ResponseInterface
+ */
+//function ($request, $response, $next)
+//{
+//	$response->getBody()->write('BEFORE');
+//	$response = $next($request, $response);
+//	$response->getBody()->write('AFTER');
+//
+//	return $response;
+//};
+//
+//function validateToken()
+//{
+//	try
+//	{
+//		$verifier = new JWTVerifier(['valid_audiences' => ['https://192.168.41.75/edeninfo'],
+//		                             'authorized_iss'  => ['https://spotlightmartdev.auth0.com/'],
+//		                             'supported_algs'  => ['RS256'],
+//		                             'cache'           => new FileSystemCacheHandler() // This parameter is optional. By default no cache is used to fetch the Json Web Keys.]);
+//		]);
+//
+//		$headers = getallheaders();
+//		$decoded = $verifier->verifyAndDecode($headers['Authorization']);
+//	} catch
+//	(InvalidTokenException $e)
+//	{
+//		deny_access();
+//	} catch (CoreException $e)
+//	{
+//		deny_access();
+//	} finally
+//	{
+//		var_dump($decoded);
+//	}
+//};
+//
+///**
+// * Deny Access
+// *
+// */
+//function deny_access()
+//{
+//	$res = $this->app->response();
+//	$res->status(401);
+//	return $res;
+//};
--- src/routes.php
+++ src/routes.php
@@ -4,21 +4,26 @@
 // For Testing only
 //var_dump($_SERVER);
 
-$app->get(
-    '/edeninfo/version', function ($request, $response, $args) {
-    $this->logger->info("version '/' route");
-	$versionSetting = $this->get('settings')['VERSION'];
-	$buildSetting = $this->get('settings')['BUILD'];
-    $myEDENInfo = new \API\EDENInfo($this->logger, $versionSetting, $buildSetting);
+$app->group('', function(){
+	$this->get(
+		'/edeninfo/version', function ($request, $response, $args) {
+		$this->logger->info("version '/' route");
+		$versionSetting = $this->get('settings')['VERSION'];
+		$buildSetting = $this->get('settings')['BUILD'];
+		$myEDENInfo = new \API\EDENInfo($this->logger, $versionSetting, $buildSetting);
 
-    return $response->withJson($myEDENInfo->getVersion());
-});
+		return $response->withJson($myEDENInfo->getVersion());
+	});
+})->add(new Middleware($container));
 
-$app->get(
-    '/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("EDENInfo:catch-all '/' route");
 
-    // Render index view
-    return $this->renderer->render($response, 'index.phtml', $args);
-});
+// This is not needed as we don't want to provide any information about our API's except to registered users.
+//
+//$app->get(
+//    '//edeninfo/[{name}]', function ($request, $response, $args) {
+//    // Sample log message
+//    $this->logger->info("EDENInfo:catch-all '/' route");
+//
+//    // Render index view
+//    return $this->renderer->render($response, 'index.phtml', $args);
+//});
