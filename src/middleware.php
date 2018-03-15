4bebc778a253fb7d0d3a8d58529b5d5053d06a4d
Scott Yacko <scott@yackofamily.com>
2018-03-14 18:31:40 -0700

    Cleaned up the composer.json, Added Auth0
    Did a Dump-Autoload to clean up the class files.
    Added Auth0 library to support Auth0 Token authorization of mobile calls. Still have to clean up the code and make it production ready.

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,26 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+
+use Auth0\SDK\JWTVerifier;
+use Auth0\SDK\Helpers\Cache\FileSystemCacheHandler;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Interop\Container\ContainerInterface;
+
+$headers =  getallheaders();
+echo $headers['Authorization'], "\n\n\n";
+
+$verifier = new JWTVerifier([
+	'valid_audiences' => ['https://192.168.41.75/edeninfo'],
+	'authorized_iss' => ['https://spotlightmartdev.auth0.com/'],
+	'supported_algs' => ['RS256'],
+	'cache' => new FileSystemCacheHandler() // This parameter is optional. By default no cache is used to fetch the Json Web Keys.
+
+]);
+
+$decoded = $verifier->verifyAndDecode($headers['Authorization']);
+
+var_dump($decoded);
+
